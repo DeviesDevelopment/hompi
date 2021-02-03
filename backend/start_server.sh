e6448603247434a26ff249fc7d65fd3dc3e125a5
Gustav Sundin <sundin666@gmail.com>
2021-02-03 15:13:16 +0100

    Autocreate superuser + docker fixes

--- backend/start_server.sh
+++ backend/start_server.sh
@@ -4,5 +4,10 @@ pip install -r requirements.txt
 
 python manage.py migrate
 
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
+    echo "Creating superuser account"
+    python manage.py createsuperuser --no-input
+fi
+
 echo "Starting server"
-python manage.py runserver 0.0.0.0:$PORT
+exec python manage.py runserver 0.0.0.0:$PORT
