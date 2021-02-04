e763f91d103f52613a45b47e13ec213df70ed6a8
Gustav Sundin <sundin666@gmail.com>
2021-02-04 11:43:02 +0100

    Change back to working state

--- backend/src/api/views.py
+++ backend/src/api/views.py
@@ -1,22 +1,12 @@
 from rest_framework import generics
+
 from .models import Task
 from .serializers import TaskSerializer
-from django.contrib.auth.mixins import LoginRequiredMixin
 
-class ListTask(LoginRequiredMixin,generics.ListCreateAPIView):
+class ListTask(generics.ListCreateAPIView):
+    queryset = Task.objects.all().order_by("due_date")
     serializer_class = TaskSerializer
 
-    def get_queryset(self):
-        return Task.objects.filter(user=self.request.user).order_by("due_date")
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
-class DetailTask(LoginRequiredMixin,generics.RetrieveUpdateDestroyAPIView):
+class DetailTask(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
     serializer_class = TaskSerializer
-
-    def get_queryset(self):
-        return Task.objects.filter(user=self.request.user)
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
