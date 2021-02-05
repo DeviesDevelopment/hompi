d48f646d4ce5613d9d9a6306bf48e88a4b6bdc7b
Gustav Sundin <sundin666@gmail.com>
2021-02-05 08:16:34 +0100

    Return only users own tasks

--- backend/src/api/views.py
+++ backend/src/api/views.py
@@ -7,6 +7,12 @@ class ListTask(generics.ListCreateAPIView):
     queryset = Task.objects.all().order_by("due_date")
     serializer_class = TaskSerializer
 
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user).order_by("due_date")
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
 class DetailTask(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
